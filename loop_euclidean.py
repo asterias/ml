df79af2a1af36f1b1254f242ee0ce91f1c76f825
asterias <nikolasm@gmail.com>
2015-11-07 17:13:17 -0500

    Added standard deviation bars

--- loop_euclidean.py
+++ loop_euclidean.py
@@ -7,6 +7,7 @@ import sys
 
 dimensions = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100]
 avg_ratios_list = []
+std_ratios_list = []
 for n in dimensions:
 	k = 100
 	s = (k,n)
@@ -54,8 +55,9 @@ for n in dimensions:
 	print "[+] Average ratio is: ", np.mean(r)
 	print "==================================================================="
 	print "[+] Standard deviation of ratios is: ", np.std(r)
-	print "==================================================================="
+	print "*******************************************************************"
 	avg_ratios_list.append(np.mean(r))
+	std_ratios_list.append(np.std(r))
 print "###################################################################"
 print "The list of all average ratios is: \n", avg_ratios_list
 print "###################################################################"
@@ -64,6 +66,8 @@ print "###################################################################"
 #print "%% \n", list_to_plot, "\n %%"  
 plt.plot(dimensions,avg_ratios_list, 'ro')
 plt.axis([0, 100, 0, 1])
+plt.errorbar(dimensions,avg_ratios_list, yerr = std_ratios_list, linestyle='none')
 plt.ylabel('Average of ratios')
 plt.xlabel('Dimensions')
+plt.title('Average ratios for K=%d'%(k))
 plt.show()
--- loop_maxNorm.py
+++ loop_maxNorm.py
@@ -7,6 +7,7 @@ import sys
 
 dimensions = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100]
 avg_ratios_list = []
+std_ratios_list = []
 for n in dimensions:
 	k = 100
 	s = (k,n)
@@ -51,20 +52,24 @@ for n in dimensions:
 	#print "==================================================================="
 	#print "[+] Ratios are: \n", r
 	print "*******************************************************************"
+	print "[+] For " , k, " points and ", n, " dimensions we have: "
+	print "==================================================================="
 	print "[+] Average ratio is: ", np.mean(r)
 	print "==================================================================="
 	print "[+] Standard deviation of ratios is: ", np.std(r)
-	print "==================================================================="
+	print "*******************************************************************"
 	avg_ratios_list.append(np.mean(r))
+	std_ratios_list.append(np.std(r))
 print "###################################################################"
 print "The list of all average ratios is: \n", avg_ratios_list
 print "###################################################################"
+
 #list_to_plot = zip(dimensions,avg_ratios_list)
 #print "%% \n", list_to_plot, "\n %%"  
 plt.plot(dimensions,avg_ratios_list, 'ro')
 plt.axis([0, 100, 0, 1])
+plt.errorbar(dimensions,avg_ratios_list, yerr = std_ratios_list, linestyle='none')
 plt.ylabel('Average of ratios')
 plt.xlabel('Dimensions')
+plt.title('Average ratios for K=100')
 plt.show()
-
-
